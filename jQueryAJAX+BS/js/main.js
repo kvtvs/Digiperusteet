a23af31be5dc28d9028e8e6f7217301bf9c9842b
kvtvs <75019687+kvtvs@users.noreply.github.com>
2021-10-13 18:33:46 +0300

    fifth commit
    fifth commit

---
+++ jQueryAJAX+BS/js/main.js
@@ -0,0 +1,68 @@
+'use strict';
+
+
+const apiURL = 'https://api.tvmaze.com/search/shows?';
+
+const hae = function(event ){
+    event.preventDefault();
+    $('#tulos').empty();
+
+    // console.log($(this).serialize());
+    $.getJSON(apiURL + $(this).serialize(), function(response){
+        //console.log(response);
+        $.each(response, function(indeksi, sarja){
+            console.log(sarja.show);
+            $('#tulos').append(`<article class="col d-flex flex-column align-items-center mt-2 mb-5">
+            <h2>${sarja.show.name}</h2>
+            <p>${sarja.show.genres}</p>
+            <a href="${sarja.show.officialSite ? sarja.show.officialSite : sarja.show.url}" class="mb-1"><i>Linkki kotisivulle</i></a>
+            <figure>
+                <img src="${sarja.show.image ? sarja.show.image.medium : 'img/tile.png'}" alt="${sarja.show.name}">
+                <figcaption>${sarja.show.name}</figcaption>
+            </figure>
+            <p>${sarja.show.summary}</p>
+            </article>`);
+        });
+    });
+};
+
+$('form').submit(hae);
+
+
+/* "use strict";
+
+const apiURL = "https://api.tvmaze.com/search/shows?";
+
+const hae = function (event) {
+  event.preventDefault();
+  $("#tulos").empty();
+  $.getJSON(apiURL + $(this).serialize(), function (response) {
+    $("#tulos").append("<h1>Tulokset</h1>");
+    if (response.length > 0) {
+      $.each(response, function (indeksi, sarja) {
+        console.log(sarja.show);
+        $("#tulos").append(`<article>
+    <h2>${sarja.show.name}</h2>
+    <p>${sarja.show.genres}</p>
+    <a href="${
+      sarja.show.officialSite ? sarja.show.officialSite : sarja.show.url
+    }">Linkki kotisivulle</a>
+    <figure>
+      <img src="${
+        sarja.show.image ? sarja.show.image.medium : "tile.png"
+      }" alt="${sarja.show.name}">
+      <figcaption>${sarja.show.name}</figcaption>
+    </figure>
+    <p>${sarja.show.summary}</p>
+  </article>`);
+      });
+    } else {
+        $("#tulos").append("<h1>Ei tuloksia</h1>");
+    }
+  }).fail(function () {
+    $("#tulos").append("<h1>Virhe</h1>");
+  });
+};
+
+$("form").submit(hae);
+ */
